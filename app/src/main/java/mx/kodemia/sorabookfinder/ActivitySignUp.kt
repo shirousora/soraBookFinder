25931f19b48265a1a825dc662b565a6d133168c1
shirousora <soraya.islas@alumnos.udg.mx>
2022-01-19 03:15:12 -0600

    first commit with login page and the beginning of the sign up page all with Material 3

---
+++ app/src/main/java/mx/kodemia/sorabookfinder/ActivitySignUp.kt
@@ -0,0 +1,11 @@
+package mx.kodemia.sorabookfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
