98ec5975db0a738ebbbc53499332197eda348d99
shirousora <soraya.islas@alumnos.udg.mx>
2022-01-20 14:49:52 -0600

    changes with new screen for signup but Home incomplete.

--- app/src/main/java/mx/kodemia/sorabookfinder/ActivityHome.kt
+++ app/src/main/java/mx/kodemia/sorabookfinder/ActivityHome.kt
@@ -3,10 +3,9 @@ package mx.kodemia.sorabookfinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ActivitySignUp : AppCompatActivity() {
+class ActivityHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_home)
     }
-
 }
--- app/src/main/java/mx/kodemia/sorabookfinder/MainActivity.kt
+++ app/src/main/java/mx/kodemia/sorabookfinder/MainActivity.kt
@@ -1,15 +1,28 @@
 package mx.kodemia.sorabookfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val buttonActivity: Button = findViewById(R.id.textButtonRegistro)
+        buttonActivity.setOnClickListener{
+            startActivity(Intent(this, ActivitySignUp::class.java))
+            Toast.makeText(this,"ir a pagina registro", Toast.LENGTH_SHORT).show()
+        }
+
+        Toast.makeText(this,"Metodo onCreate", Toast.LENGTH_SHORT).show()
+
     }
+
+
 
 
 }
